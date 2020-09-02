4f4e50223df1fc46f851704fba7a035d582919f8
mbleandro <xinalele@gmail.com>
2020-09-01 21:39:21 -0300

    map

---
+++ app/src/app/components/ol-map/ol-map.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, AfterViewInit, ElementRef, Input } from '@angular/core';
+
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import XYZ from 'ol/source/XYZ';
+import { OSM } from 'ol/source';
+import * as Proj from 'ol/proj';
+import {
+  defaults as defaultControls,
+  Control
+} from 'ol/control';
+import { LoadingController } from '@ionic/angular';
+
+export const DEFAULT_HEIGHT = '500px';
+export const DEFAULT_WIDTH = '500px';
+
+@Component({
+  selector: 'app-ol-map',
+  templateUrl: './ol-map.component.html',
+  styleUrls: ['./ol-map.component.scss'],
+})
+export class OlMapComponent implements OnInit, AfterViewInit {
+
+  @Input() lat: number;
+  @Input() lon: number;
+  @Input() zoom: number;
+  @Input() width: string | number = DEFAULT_WIDTH;
+  @Input() height: string | number = DEFAULT_HEIGHT;
+
+  map: Map;
+
+  private mapElement: HTMLElement;
+  constructor(
+    private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.mapElement = this.elementRef.nativeElement.querySelector('#map');
+    this.setSize();
+  }
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: Proj.fromLonLat([this.lon, this.lat]),
+        zoom: this.zoom
+      }),
+      controls: defaultControls().extend([])
+    });
+  }
+
+  private setSize(): void {
+    if (this.mapElement) {
+      const styles = this.mapElement.style;
+      styles.height = coerceCssPixelValue(this.height) || DEFAULT_HEIGHT;
+      styles.width = coerceCssPixelValue(this.width) || DEFAULT_WIDTH;
+    }
+  }
+}
+
+const cssUnitsPattern = /([A-Za-z%]+)$/;
+function coerceCssPixelValue(value: any): string {
+  if (value == null) {
+    return '';
+  }
+  return cssUnitsPattern.test(value) ? value : `${value}px`;
+}
